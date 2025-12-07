f04b7dd864217b45fbae7face6ec4a151dfb3ca6
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-12-07 23:49:01 +0530

    Update script.js

--- 02_DataType_TypeSystem/script.js
+++ 02_DataType_TypeSystem/script.js
@@ -1,3 +1,5 @@
 // primitives => aisi saari values jinko copy karna par ek real copy mil jaaye
+Ex = string, number, boolean, null, undefined, symbol, bigint
 
 // reference => inko copy karan par real copy nahi milegi but reference milega parent ka
+
